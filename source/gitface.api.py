bde7ccf5c00f4fa1f0515d33c5d656e578a610bb
Kellen Fujimoto <kellen@kellenfujimoto.com>
2013-05-17 18:39:43 -0700

    Basic crypto
    Some edits at work. Added encryption to Entry post method, etc.

--- source/gitface.api.py
+++ source/gitface.api.py
@@ -2,10 +2,16 @@
 
 import os
 import sqlite3
+from Crypto.PublicKey import RSA
+import zlib
+
+# Constants
+# Might toss all of these into a configuration file. Once it gets too verbose.
+gitfaceDir = os.path.expanduser(os.path.normcase("~/gitface")) # Protect the Windows users' paths
 
 # Convenience functions
 # Connecting to a database and executing something
-def dbExec(target, statements, encrypt):
+def dbExec(target, statements):
 	# target is the string of the absolte path to the database you want to write to
 	# statements is a list of sql statements (make sure to add the ";" at the end; you aren't in Kansas anymore!)
 	conn = sqlite3.connect(target)
@@ -14,16 +20,21 @@ def dbExec(target, statements, encrypt):
 		c.execute(command)
 	conn.commit()
 	c.close()
-	return c
+	return c # Make dbExec() work with 'select' statements
 
-###############
-# Data Models #
-###############
-class Database(object):
-	pass
+def keyFetch(keyRing, keyDBPath = gitfaceDir + "keys.db", kind):
+	# Digs into key database and fetches either public or private key
+	if kind is "private":
+		ringKey = dbExec(keyDBPath, ["from keys select private where ring is" + str(keyRing) + ";"])
+	else:
+		ringKey = dbExec(keyDBPath, ["from keys select public where ring is" + str(keyRing) + ";"])
+	return ringKey
 
-class Stream(Database):
-	def __init__(self, path, gitfaceDB = "gitface.db", keyDB = "keys.db", privateKey):
+# Class definitions
+
+class Stream(object):
+	""" Database stream of entries """
+	def __init__(self, path = gitfaceDir, gitfaceDB = "gitface.db", keyDB = "keys.db", privateKey):
 		self.path = path
 		self.gitfaceDB = gitfaceDB
 		self.keyDB = keyDB
@@ -33,12 +44,13 @@ class Stream(Database):
 			os.makedirs(self.path)
 		f = open(self.path + self.gitfaceDB, "w+", 0)
 		f.close()
-		dbExec(self.path + self.name, ["create table if not exists stream (ring integer, metadata text, date real, body text);", "create table if not exists rings (ring integer, key text, description text);"])
+		dbExec(self.path + self.name, ["create table if not exists stream (ring integer, data text, date real);", "create table if not exists rings (ring integer, priKey text, pubKey text, description text);"])
 
 		# Check if keys database and table exist, create if not
 		f = open(self.path + self.keyDB, "w+", 0)
 		f.close()
-		dbExec(self.path + self.keyDB, ["create table if not exists keys (key text, description text);", "insert into keys values (0, '', 'The public ring; unencrypted and insecure');", "insert into keys values (1, " + privateKey + ", 'Your private data; do not share this key with anyone!');"])
+		# Need to generate public and private keys for ring 1
+		dbExec(self.path + self.keyDB, ["create table if not exists keys (ring text, public text, description text);", "insert into keys values (0, '', 'The public ring; unencrypted and insecure');", "insert into keys values (1, " + pubKey + ", 'Your private data; do not share the private key with anyone!')"])
 
 	def command(self, sql):
 		if os.path.exists(self.path) == False:
@@ -46,31 +58,21 @@ class Stream(Database):
 			f = open(self.name, "w+", 0)
 			f.close()
 
-		dbExec(self.path + self.name, sql)
-		return True
+		r = dbExec(self.path + self.name, sql)
+		return r
 
 class Entry(object):
-	def __init__(self, timestamp = datetime.utcnow(), location):
+	def __init__(self, timestamp = datetime.utcnow(), location, ring = 1):
 		self.location = location # Where the post lives, aka the absolute path to the stream database
 		self.timestamp = timestamp
-		self.metadata = {ring: 1}
-		self.body = ""
+		self.ring = ring
+		self.pubKey = RSA.RSAImplimentation.importKey(keyFetch(keyRing = ring))
+		self.entryID = dbExec(self.location, ["insert into stream values (" + str(self.ring) + ", '', " + self.timestamp + ");"])
 
-	def update(args):
-		# args should be a comma-seperated list of attribute/value pairs
+	def post(args):
+		# args should be a tuple with alternating names and values
 		# it's an ugly imitation of named parameters, but it'll do
-		for key in args.keys():
-			if key.lower() == "ring":
-				self.metadata["ring"] = args[key]
-			elif key.lower() == "body":
-				self.body == args[key]
-			else:
-				self.metadata[key.lower()] = args[key]
-
-	def post(self, target = self.location):
-		sql =
-		dbExec(target, )
-
-class EncryptedStream(object):
-	def __init__(self, input):
-		import
+		data = self.pubKey.encrypt(zlib.compress(args, 9), K)
+		dbExec(self.location, ["update stream set data=" + str(data) + " where timestamp=" + self.timestamp + ";"])
+		delattr(args)
+		return True
